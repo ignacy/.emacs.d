497503ec7546b254c8a04e30d1cb0382df114304
Ignacy Moryc <imoryc@gmail.com>
2010-10-11 14:56:11 +0200

    Oguras mode

--- init.el
+++ init.el
@@ -4,6 +4,10 @@
 
 (load-file (concat dotfiles-dir "/haskell-mode/haskell-site-file.el"))
 
+(add-to-list 'load-path (concat dotfiles-dir "/feature-mode"))
+(require 'feature-mode)
+(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
+
 
 (setq ditaa-cmd "java -jar /home/ignacy/bin/ditaa0_9.jar")
 (defun djcb-ditaa-generate ()
@@ -223,32 +227,36 @@
 
 (setq font-use-system-font t)
 
-(require 'flymake)
+;; (require 'flymake)
 
-;; I don't like the default colors :)
-(set-face-background 'flymake-errline "red4")
-(set-face-background 'flymake-warnline "dark slate blue")
+;; ;; I don't like the default colors :)
+;; (set-face-background 'flymake-errline "red4")
+;; (set-face-background 'flymake-warnline "dark slate blue")
 
-;; Invoke ruby with '-c' to get syntax checking
-(defun flymake-ruby-init ()
-  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-	 (local-file  (file-relative-name
-                       temp-file
-                       (file-name-directory buffer-file-name))))
-    (list "ruby" (list "-c" local-file))))
+;; ;; Invoke ruby with '-c' to get syntax checking
+;; (defun flymake-ruby-init ()
+;;   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;; 	 (local-file  (file-relative-name
+;;                        temp-file
+;;                        (file-name-directory buffer-file-name))))
+;;     (list "ruby" (list "-c" local-file))))
 
-(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+;; (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+;; (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
 
-(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+;; (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
 
-(add-hook 'ruby-mode-hook
-          '(lambda ()
+;; (add-hook 'ruby-mode-hook
+;;           '(lambda ()
 
-	     ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
-	     (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
-		 (flymake-mode))
-	     ))
+;; 	     ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
+;; 	     (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
+;; 		 (flymake-mode))
+;; 	     ))
 
 
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
