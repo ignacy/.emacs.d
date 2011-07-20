042ca806dd1fe14362f9612c74947d74639dfedf
Ignacy Moryc <imoryc@gmail.com>
2011-07-20 18:56:26 +0200

    Fixed setup

--- init.el
+++ init.el
@@ -767,3 +767,40 @@ This is the same as using \\[set-mark-command] with the prefix argument."
 (add-to-list 'load-path (concat dotfiles-dir "/textmate.el"))
 (require 'textmate)
 (textmate-mode)
+
+
+(defadvice erase-buffer (around erase-buffer-noop)
+  "make erase-buffer do nothing")
+
+(defadvice shell-command (around shell-command-unique-buffer activate compile)
+  (if (or current-prefix-arg
+          (not (string-match "[ \t]*&[ \t]*\\'" command)) ;; background
+          (bufferp output-buffer)
+          (stringp output-buffer))
+      ad-do-it ;; no behavior change
+
+    ;; else we need to set up buffer
+    (let* ((command-buffer-name
+            (format "*background: %s*"
+                    (substring command 0 (match-beginning 0))))
+           (command-buffer (get-buffer command-buffer-name)))
+
+      (when command-buffer
+        ;; if the buffer exists, reuse it, or rename it if it's still in use
+        (cond ((get-buffer-process command-buffer)
+               (set-buffer command-buffer)
+               (rename-uniquely))
+              ('t
+               (kill-buffer command-buffer))))
+      (setq output-buffer command-buffer-name)
+
+      ;; insert command at top of buffer
+      (switch-to-buffer-other-window output-buffer)
+      (insert "Running command: " command
+              "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+
+      ;; temporarily blow away erase-buffer while doing it, to avoid
+      ;; erasing the above
+      (ad-activate-regexp "erase-buffer-noop")
+      ad-do-it
+      (ad-deactivate-regexp "erase-buffer-noop"))))
