a05d3211e62c245fa665dc3426e1049592982c14
Ignacy Moryc <imoryc@gmail.com>
2011-09-02 08:12:50 +0200

    modeline customization

--- init.el
+++ init.el
@@ -538,7 +538,10 @@ instead."
 (unless on-windows
   (setq rsense-home "/home/ignacy/bin/rsense-0.3")
   (add-to-list 'load-path (concat rsense-home "/etc"))
-  (require 'rsense))
+  (require 'rsense)
+  (add-to-list 'ac-sources 'ac-source-rsense-method)
+  (add-to-list 'ac-sources 'ac-source-rsense-constant)
+  )
 
 (define-key isearch-mode-map (kbd "C-o")
   (lambda ()
@@ -764,7 +767,7 @@ This is the same as using \\[set-mark-command] with the prefix argument."
 
 
 ;;(set-face-attribute 'default nil :font "Mono Dyslexic-13")
-(set-face-attribute 'default nil :font "Inconsolata-13")
+;;(set-face-attribute 'default nil :font "Inconsolata-13")
 
 
 (when on-windows
@@ -896,3 +899,65 @@ i.e. in daylight or under bright electric lamps."
    (backward-char)))
 
 (bind "<f6>" im/worklog-add)
+
+
+
+;; use setq-default to set it for /all/ modes
+(setq mode-line-format
+      (list
+       ;; the buffer name; the file name as a tool tip
+       '(:eval (propertize "%b " 'face 'font-lock-keyword-face
+                           'help-echo (buffer-file-name)))
+
+       ;; line and column
+       "(" ;; '%02' to set to 2 chars at least; prevents flickering
+       (propertize "%02l" 'face 'font-lock-type-face) ","
+       (propertize "%02c" 'face 'font-lock-type-face)
+       ") "
+
+       ;; relative position, size of file
+       "["
+       (propertize "%p" 'face 'font-lock-constant-face) ;; % above top
+       "/"
+       (propertize "%I" 'face 'font-lock-constant-face) ;; size
+       "] "
+
+       ;; the current major mode for the buffer.
+       "["
+
+       '(:eval (propertize "%m" 'face 'font-lock-string-face
+                           'help-echo buffer-file-coding-system))
+       "] "
+
+
+       "[" ;; insert vs overwrite mode, input-method in a tooltip
+       '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
+                           'face 'font-lock-preprocessor-face
+                           'help-echo (concat "Buffer is in "
+                                              (if overwrite-mode "overwrite" "insert") " mode")))
+
+       ;; was this buffer modified since the last save?
+       '(:eval (when (buffer-modified-p)
+                 (concat ","  (propertize "Mod"
+                                          'face 'font-lock-warning-face
+                                          'help-echo "Buffer has been modified"))))
+
+       ;; is this buffer read-only?
+       '(:eval (when buffer-read-only
+                 (concat ","  (propertize "RO"
+                                          'face 'font-lock-type-face
+                                          'help-echo "Buffer is read-only"))))
+       "] "
+
+       ;; add the time, with the date and the emacs uptime in the tooltip
+       '(:eval (propertize (format-time-string "%H:%M")
+                           'help-echo
+                           (concat (format-time-string "%c; ")
+                                   (emacs-uptime "Uptime:%hh"))))
+       " --"
+       ;; i don't want to see minor-modes; but if you want, uncomment this:
+       ;; minor-mode-alist  ;; list of minor modes
+       "%-" ;; fill with '-'
+       ))
+
+(set-face-background 'modeline "#001A4C")
