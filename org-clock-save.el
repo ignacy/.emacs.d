901a0aa4c7f12586c389986fbf32ab62fcf14c33
Ignacy Moryc <imoryc@gmail.com>
2011-10-02 19:19:31 +0200

    Add longer interval for deft

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,1 @@
-;; org-persist.el - IGNACY-KOMPUTER at <2011-09-28 ¦r 14:10>
-(setq stored-clock-history '(("c:/Users/Ignacy/Dropbox/notes/deft/deft-0.org" . 2468) ("c:/Users/Ignacy/Dropbox/notes/deft/deft-0.org" . 1267) ("c:/Users/Ignacy/Dropbox/notes/deft/deft-0.org" . 1800)))
+;; org-persist.el - eckhart at <2011-10-02 nie 19:18>
