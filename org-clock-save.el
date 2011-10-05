f316c524625a0da8945c3a5c67c3eadca6ea2258
Ignacy Moryc <imoryc@gmail.com>
2011-10-05 10:48:39 +0200

    A few changes

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - IGNACY-KOMPUTER at <2011-10-03 Pn 08:25>
-(setq stored-clock-history '(("c:/Users/Ignacy/Dropbox/notes/deft/deft-0.org" . 2511) ("c:/Users/Ignacy/Dropbox/notes/deft/deft-0.org" . 1267) ("c:/Users/Ignacy/Dropbox/notes/deft/deft-0.org" . 1875) ("c:/Users/Ignacy/Dropbox/notes/deft/deft-0.org" . 1441)))
+;; org-persist.el - IGNACY-KOMPUTER at <2011-10-03 Pn 10:05>
+(setq stored-clock-history '(   ))
