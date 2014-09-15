1da42724a943bf0dcf08d60fbe78bd9f926eda22
Ignacy Moryc <ignacy.moryc@up-next.com>
2014-09-15 21:00:58 +0200

    Don't highlight magit's diffs

--- elisp/custom.el
+++ elisp/custom.el
@@ -6,12 +6,12 @@
  '(background-mode dark)
  '(bmkp-last-as-first-bookmark-file "~/.bookmarks_emacs")
  '(rspec-use-bundler-when-possible t)
- '(rspec-use-spring-when-possible nil)
-)
+ '(rspec-use-spring-when-possible nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-dim-other-buffers-face ((t (:background "gray10")))))
+ '(auto-dim-other-buffers-face ((t (:background "gray10"))))
+ '(magit-item-highlight ((t nil))))
