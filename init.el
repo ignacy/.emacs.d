fd419f05ad5eb40031d68273beb831d2b3db4912
Ignacy Moryc <ignacy.moryc@up-next.com>
2014-10-22 23:04:29 +0200

    sk

--- init.el
+++ init.el
@@ -56,5 +56,17 @@
           (setq rbenv-show-active-ruby-in-modeline nil)
           (global-rbenv-mode)))
 
-(require 'color-identifiers-mode)
-(add-hook 'after-init-hook 'global-color-identifiers-mode)
+(use-package color-identifiers-mode
+  :init
+  (progn
+    (let ((faces '(font-lock-comment-face font-lock-comment-delimiter-face font-lock-constant-face font-lock-type-face font-lock-function-name-face font-lock-variable-name-face font-lock-keyword-face font-lock-string-face font-lock-builtin-face font-lock-preprocessor-face font-lock-warning-face font-lock-doc-face)))
+      (dolist (face faces)
+        (set-face-attribute face nil :foreground nil :weight 'normal :slant 'normal)))
+
+    (set-face-attribute 'font-lock-comment-delimiter-face nil :slant 'italic)
+    (set-face-attribute 'font-lock-comment-face nil :slant 'italic)
+    (set-face-attribute 'font-lock-doc-face nil :slant 'italic)
+    (set-face-attribute 'font-lock-keyword-face nil :weight 'bold)
+    (set-face-attribute 'font-lock-builtin-face nil :weight 'bold)
+    (set-face-attribute 'font-lock-preprocessor-face nil :weight 'bold)
+    (add-hook 'after-init-hook 'global-color-identifiers-mode)))
