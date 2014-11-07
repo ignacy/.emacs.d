0d2c92a7fc1a99a76428d82eb3b6782c7604dfe7
Ignacy Moryc <imoryc@gmail.com>
2014-11-07 13:15:07 +0100

    Some go settings

---
+++ elisp/init-go-mode.el
@@ -0,0 +1,18 @@
+(use-package go-mode)
+(use-package go-eldoc)
+(use-package golint)
+(use-package go-mode-load
+  :init (progn
+          (defun my-go-mode-hook ()
+            (add-hook 'before-save-hook 'gofmt-before-save)
+            ;;(if (not (string-match "go" compile-command))
+            (set (make-local-variable 'compile-command)
+                 "go vet && go test -cover -v && go build -v")
+            ;;                )
+
+            (setq tab-width 8 indent-tabs-mode 1)
+            (local-set-key (kbd "M-.") 'godef-jump))
+          (add-hook 'go-mode-hook 'my-go-mode-hook)
+          (add-hook 'go-mode-hook 'go-eldoc-setup)))
+
+(provide 'init-go-mode)
