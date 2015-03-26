f92fb49812476fed50dc1504ea7ece6ddd319f85
Ignacy Moryc <imoryc@gmail.com>
2015-03-26 12:38:57 +0100

    Remove unused stuff

--- elisp/helpers.el
+++ elisp/helpers.el
@@ -453,58 +453,8 @@ narrowed."
   (switch-to-buffer "*ssh*"))
 
 
-(defun eshell-here ()
-  "Opens up a new shell in the directory associated with the
-current buffer's file. The eshell is renamed to match that
-directory to make multiple eshell windows easier."
-  (interactive)
-  (let* ((parent (if (buffer-file-name)
-                     (file-name-directory (buffer-file-name))
-                   default-directory))
-         (height (/ (window-total-height) 3))
-         (name   (car (last (split-string parent "/" t)))))
-    (split-window-vertically (- height))
-    (other-window 1)
-    (eshell "new")
-    (rename-buffer (concat "*eshell: " name "*"))
-
-    (insert (concat "ls"))
-    (eshell-send-input)))
-
-(global-set-key (kbd "C-!") 'eshell-here)
-
-
-(defun zap-to-isearch (rbeg rend)
-  "Kill the region between the mark and the closest portion of
-the isearch match string. The behaviour is meant to be analogous
-to zap-to-char; let's call it zap-to-isearch. The deleted region
-does not include the isearch word. This is meant to be bound only
-in isearch mode.  The point of this function is that oftentimes
-you want to delete some portion of text, one end of which happens
-to be an active isearch word. The observation to make is that if
-you use isearch a lot to move the cursor around (as you should,
-it is much more efficient than using the arrows), it happens a
-lot that you could just delete the active region between the mark
-and the point, not include the isearch word."
-  (interactive "r")
-  (when (not mark-active)
-    (error "Mark is not active"))
-  (let* ((isearch-bounds (list isearch-other-end (point)))
-         (ismin (apply 'min isearch-bounds))
-         (ismax (apply 'max isearch-bounds))
-         )
-    (if (< (mark) ismin)
-        (kill-region (mark) ismin)
-      (if (> (mark) ismax)
-          (kill-region ismax (mark))
-        (error "Internal error in isearch kill function.")))
-    (isearch-exit)
-    ))
-
-(define-key isearch-mode-map [(meta z)] 'zap-to-isearch)
-
-
 (require 'term-run)
+
 (defun git-last-30-commits ()
   (interactive)
   (term-run-shell-command "git log --oneline -30"))
