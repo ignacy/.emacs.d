90eacd92ddebba7ce3d5405ad4213fe10c276acc
Ignacy Moryc <ignacy.moryc@up-next.com>
2015-04-01 20:17:40 +0200

    theme mods

--- themes/default-black-theme.el
+++ themes/default-black-theme.el
@@ -13,13 +13,14 @@
 
 (custom-theme-set-faces
  'default-black
- '(default ((t (:inherit nil :stipple nil :background "Black" :foreground "White" :inverse-video nil :box nil :strike-t*hrough nil :overline nil :underline nil :slant normal :weight normal :width normal :height 110))))
+ '(default ((t (:inherit nil :stipple nil :background "Black" :foreground "White" :inverse-video nil :box nil :strike-t*hrough nil :overline nil :underline nil :slant normal :weight normal :width normal :height 140))))
  '(highlight ((((class color) (min-colors 88) (background dark)) (:background "#111111"))))
  '(region ((nil (:background "#464740"))))
  '(hl-line ((nil (:background "#222222"))))
  '(yas-field-highlight-face ((nil (:background "#333399"))))
  '(js2-function-param-face ((t (:foreground "LightGoldenrod"))))
  '(font-lock-comment-face ((t (:foreground "#808080"  ))))
+ '(font-lock-string-face ((t (:foreground "DarkSeaGreen3"  ))))
  '(font-lock-warning-face ((nil (:foreground "#ff6666"))))
  '(show-paren-match ((nil (:background "#333399"))))
  '(show-paren-mismatch ((((class color)) (:background "red")))))
