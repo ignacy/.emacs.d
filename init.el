146de7ab0e83aedd664ad764ed537883402d7cac
Ignacy Moryc <ignacy.moryc@up-next.com>
2015-08-15 09:02:31 +0200

    Remove ANSI color settings for term mode
    Why I have to do that at all is a good question, probably there were
    some problems with a color theme or sth like that.

--- init.el
+++ init.el
@@ -341,8 +341,6 @@
 (setq ruby-deep-indent-paren-style nil)
 (setq ruby-deep-indent-paren nil)
 
-(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
-
 (add-hook 'shell-mode-hook
           '(lambda ()
              (setq show-trailing-whitespace nil)
@@ -382,39 +380,6 @@
 (use-package multi-term
   :ensure multi-term
   :init (setq multi-term-program "/bin/zsh"))
-
-;; term
-(defface term-color-black
-  '((t (:foreground "#3f3f3f" :background "#272822")))
-  "Unhelpful docstring.")
-(defface term-color-red
-  '((t (:foreground "#cc9393" :background "#272822")))
-  "Unhelpful docstring.")
-(defface term-color-green
-  '((t (:foreground "#7f9f7f" :background "#272822")))
-  "Unhelpful docstring.")
-(defface term-color-yellow
-  '((t (:foreground "#f0dfaf" :background "#272822")))
-  "Unhelpful docstring.")
-(defface term-color-blue
-  '((t (:foreground "#6d85ba" :background "#272822")))
-  "Unhelpful docstring.")
-(defface term-color-magenta
-  '((t (:foreground "#dc8cc3" :background "#272822")))
-  "Unhelpful docstring.")
-(defface term-color-cyan
-  '((t (:foreground "#93e0e3" :background "#272822")))
-  "Unhelpful docstring.")
-(defface term-color-white
-  '((t (:foreground "#dcdccc" :background "#272822")))
-  "Unhelpful docstring.")
-'(term-default-fg-color ((t (:inherit term-color-white))))
-'(term-default-bg-color ((t (:inherit term-color-black))))
-
-;; ansi-term colors
-(setq ansi-term-color-vector
-      [term term-color-black term-color-red term-color-green term-color-yellow
-            term-color-blue term-color-magenta term-color-cyan term-color-white])
 
 ;; shell-mode
 (defun sh (&optional name)
