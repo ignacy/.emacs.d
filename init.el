2c9797a1cee801e9201053821987abe44b74980c
Ignacy Moryc <imoryc@gmail.com>
2015-08-15 09:28:34 +0200

    Switch to compleating read

--- init.el
+++ init.el
@@ -398,7 +398,7 @@
   :ensure magit
   :bind ("C-x g" . magit-status)
   :init (progn
-          (setq magit-completing-read-function 'magit-ido-completing-read)
+          (setq magit-completing-read-function completing-read-function)
           ;; close popup when commiting
           (defun magit-commit-mode-init () (when (looking-at "\n")
                                              (open-line 1)))
