1ea54b9526b4cc65d956876e979bfdec58f23135
Ignacy Moryc <imoryc@gmail.com>
2016-02-14 10:21:48 +0100

    a

---
+++ custom.el
@@ -0,0 +1,27 @@
+;; Custom
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("f2503f0a035c2122984e90eb184185769ee665de5864edc19b339856942d2d2d" "53d91dd074ca40087e5ec47842086a5cc5b0d019fdc591089e8ebb1e56b08d4f" "59ca830d4df5e79503b79103485d28c6a578ca14d526ffc6a43596808daf1282" "8288b9b453cdd2398339a9fd0cec94105bc5ca79b86695bd7bf0381b1fbe8147" "e14f8791b180b69f95550ae9fb6b3fa2321115197908f0a8a2cd924bd5acc86f" "e24679edfdea016519c0e2d4a5e57157a11f928b7ef4361d00c23a7fe54b8e01" "76bd62f6ce376bf0597fab7f478eaa98cd94a7b41f0ae46de63a958fbe99c1d9" "16d6e7f87846801e17e0c8abc331cf6fa55bec73185a86a431aca6bec5d28a0a" "e2e4e109357cfcebccb17961950da6b84f72187ade0920a4494013489df648fe" "d1a42ed39a15a843cccadf107ee0242b5f78bfbb5b70ba3ce19f3ea9fda8f52d" "db9feb330fd7cb170b01b8c3c6ecdc5179fc321f1a4824da6c53609b033b2810" "b1bcb837df0455af8e91114b7a3bddfa084cde32ceb16b1b468d5e5e8605a835" "890d09dcc8d2326e98eee74b307b2cc42f07ab7701bcff521e6152aa3e08f7a8" "aa87469691932ff791f966bffb885ecd97ebfa4dc4d42e479f3819ac4a3fbcaf" "43aeadb0c8634a9b2f981ed096b3c7823c511d507a51c604e4667becb5ef6e35" "90b53494ee9b1434f231ec22fe418b78b4e0af8ff09314333cb2771de723c335" "01ce486c3a7c8b37cf13f8c95ca4bb3c11413228b35676025fdf239e77019ea1" "b6db49cec08652adf1ff2341ce32c7303be313b0de38c621676122f255ee46db" "50e7f9d112e821e42bd2b8410d50de966c35c7434dec12ddea99cb05dd368dd8" "f245c9f24b609b00441a6a336bcc556fe38a6b24bfc0ca4aedd4fe23d858ba31" "e8e744a1b0726814ac3ab86ad5ccdf658b9ff1c5a63c4dc23841007874044d4a" "fe1682ca8f7a255cf295e76b0361438a21bb657d8846a05d9904872aa2fb86f2" "040eab9c119c33f184e9fd21290ecea9691efc807a89c328e12a50d3a0fe60f8" "9dad4b11367cda1041ed375ae2f7cc71558898f234edf4893498c5b22b283e56" "705f3f6154b4e8fac069849507fd8b660ece013b64a0a31846624ca18d6cf5e1" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" "5a0eee1070a4fc64268f008a4c7abfda32d912118e080e18c3c865ef864d1bea" "7c996062565867025210d10a3ca9825c013251a118449d24948afc7b10470e6a" "c3d843722b3bf87f037a6d54cdf0042ce4b2150bd24af4e32eb7342f782bdd44" "b2353a94c574d4c42afa7c118511f7b68a8342f74306cfa363e93e8737f31b53" "badc4f9ae3ee82a5ca711f3fd48c3f49ebe20e6303bba1912d4e2d19dd60ec98" "195714630beff5d3ace62eb3bf89d70c723a3e44ac55a85825677231280cfca7" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1fab355c4c92964546ab511838e3f9f5437f4e68d9d1d073ab8e36e51b26ca6a" "0155b3b94f6d5bce0275a15bc662be4f6f9f3284f9e469ca4ab1bd67ff9b5222" "98a619757483dc6614c266107ab6b19d315f93267e535ec89b7af3d62fb83cad" "8a7d14b8d27268a2d0ed0015b48408af3c401d3c4642773038eb3ea56d1b6960" "8855d6dbd8b4ea5d87146dc77ba1b8b7f06142ee8107429f0520c38777d9b39e" default)))
+ '(package-selected-packages
+   (quote
+    (ir-black-theme coffee-mode reykjavik-theme suscolors-theme haml-mode rainbow-identifiers rainbow-identifiers-mode smartparens smart-parens evil-leader evil base16-theme alchemist sane-term rspec-mode projectile-rails atom-one-dark-theme hydandata-light-theme monokai-theme smyx-theme spacegray-theme fill-column-indicator apropospriate-theme gruber-darker-theme gruvbox-theme idea-darkula-theme kooten-theme paper-theme markdown-mode ## rainbow-mode color-theme-sanityinc-tomorrow quasi-monochrome-theme idomenu expand-region badwolf-theme planet-theme highlight-symbol web-mode js2-mode wrap-region zerodark-theme darktooth-theme company farmhouse-theme rubocop ruby-hash-syntax rbenv exec-path-from-shell projectile anzu flx-isearch flx-ido flx ido-vertical-mode ido-vertical magit ag ido-recentf find-file-in-repository find-file-in-project elixir-mode use-package)))
+ '(safe-local-variable-values
+   (quote
+    ((encoding . utf-8)
+     (ffip-prune-patterns
+      (quote
+       ("*.cssc" "*/.git/*" "*/node_modules/*" "*/index.js")))
+     (ffip-project-root . "~/code/bridge")))))
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(highlight-symbol-face ((t (:background "DodgerBlue4")))))
---
+++ init.el
@@ -0,0 +1,388 @@
+(setq mac-command-modifier 'meta)
+(setq inhibit-startup-message 't)
+(setq tags-add-tables nil)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(fringe-mode -1)
+(show-paren-mode 1)
+(setq ring-bell-function 'ignore)
+(delete-selection-mode t)
+(load-library "url-handlers")
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure t)
+
+(require 'use-package)
+(use-package exec-path-from-shell
+  :init (progn
+          (setq exec-path-from-shell-arguments '("-l"))
+          (when (memq window-system '(mac ns))
+            (exec-path-from-shell-initialize))))
+
+(use-package diminish)
+
+(use-package company
+  :init (progn
+          (setq company-dabbrev-downcase nil)
+          (setq company-dabbrev-ignore-case nil)
+          (global-company-mode t)
+          (diminish 'company-mode)))
+
+(setq abbrev-file-name "~/.emacs.d/abbrevations")
+(setq dabbrev-case-replace nil)
+(setq default-abbrev-mode t)
+(setq compilation-scroll-output t)
+
+
+(if (file-exists-p abbrev-file-name)
+    (quietly-read-abbrev-file))
+
+(use-package elixir-mode)
+
+(use-package alchemist)
+
+
+(ido-mode 1)
+(ido-everywhere)
+
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(setq ido-create-new-buffer 'always)
+(set-default 'imenu-auto-rescan t)
+(defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match)
+  (define-key ido-completion-map (kbd "<down>") 'ido-next-match)
+  (define-key ido-completion-map (kbd "<up>") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
+
+(use-package flx-ido
+  :init (progn
+          (flx-ido-mode 1)
+          ;; disable ido faces to see flx highlights.
+          (setq ido-enable-flex-matching t)
+          (setq ido-use-faces nil)))
+
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq make-backup-files nil)
+
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+(use-package projectile-rails)
+(use-package projectile
+  :init (progn
+          (defadvice find-tag-at-point (before auto-visti-tags)
+            "Load default TAGS file from home directory if needed"
+            (visit-tags-table (concat (projectile-project-root) "TAGS")))
+
+          (ad-activate 'find-tag-at-point)
+          (projectile-global-mode)
+          (diminish 'projectile-mode)
+          (add-hook 'projectile-mode-hook 'projectile-rails-on))
+  :bind ("C-c C-p" . projectile-switch-project))
+
+(global-set-key (kbd "C-x f") 'projectile-find-file)
+(global-set-key (kbd "C-x C-f") 'ido-find-file)
+(global-set-key (kbd "C-x b") 'projectile-switch-to-buffer)
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
+(global-set-key (kbd "C-c m") 'projectile-rails-find-current-spec)
+
+
+(use-package highlight-symbol
+  :init (progn
+          (add-hook 'prog-mode-hook 'highlight-symbol-mode)
+          (add-hook 'prog-mode-hook 'highlight-symbol-nav-mode)
+          (setq highlight-symbol-idle-delay 0)))
+
+(use-package ruby-mode
+  :init (progn
+          (add-hook 'ruby-mode-hook
+                    (lambda ()
+                      (subword-mode 1)
+                      ))))
+
+(use-package inf-ruby)
+(add-hook 'after-init-hook 'inf-ruby-switch-setup)
+
+
+(setq ruby-use-encoding-map nil)
+(setq ruby-deep-arglist nil)
+;;(setq ruby-deep-indent-paren nil)
+(setq ruby-deep-indent-paren '(?\[ ?\] t))
+(setq ruby-insert-encoding-magic-comment nil)
+(setq ruby-deep-indent-paren-style nil)
+(setq ruby-deep-indent-paren nil)
+
+(use-package ruby-hash-syntax)
+
+(use-package rubocop
+  :config (progn
+            (setq rubocop-check-command "/Users/ignacymoryc/.rbenv/shims/rubocop --format emacs")
+            (setq rubocop-autocorrect-command "/Users/ignacymoryc/.rbenv/shims/rubocop -a --format emacs")
+            ))
+
+
+(use-package rbenv
+  :init (progn
+          (setq rbenv-show-active-ruby-in-modeline nil)
+          (global-rbenv-mode)))
+
+(use-package rspec-mode
+  :ensure  rspec-mode
+  :config (progn
+            (setq rspec-use-rake-when-possible nil)
+            (setq rspec-use-rvm nil)
+            (setq rspec-use-bundler-when-possible nil)
+            ;;(setq rspec-command-options "--format progress --order random")
+            ))
+
+
+(use-package recentf
+  :init (progn
+          (setq recentf-auto-cleanup 'never)
+          (recentf-mode t)
+          (setq recentf-max-saved-items 2000)
+          (setq recentf-max-menu-items 25)
+
+          (defun recentf-ido-find-file ()
+            "Find a recent file using Ido."
+            (interactive)
+            (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+              (when file
+                (find-file file))))))
+
+(global-set-key (kbd "C-x C-r") 'recentf-ido-find-file)
+
+(use-package wrap-region
+  :init (progn
+          (wrap-region-global-mode +1)
+          (wrap-region-add-wrapper "`" "`")
+          (wrap-region-add-wrapper "{" "}")))
+
+(use-package expand-region
+  :defer t
+  :bind ("M-2" . er/expand-region))
+
+(use-package magit
+  :config (setq magit-completing-read-function 'ido-completing-read)
+  :init (global-set-key (kbd "C-x g") 'magit-status))
+
+
+(use-package ag
+  :init (global-set-key (kbd "M-r") 'ag-project))
+
+(global-auto-revert-mode 1)
+
+(global-set-key (kbd "C-<tab>") (lambda () (interactive) (switch-to-buffer (other-buffer (current-buffer) 1))))
+(global-set-key (kbd "C-x i") 'indent-region-or-buffer)
+(global-set-key (kbd "C-S-n") (lambda () (interactive) (ignore-errors (next-line 5))))
+(global-set-key (kbd "C-S-p") (lambda () (interactive) (ignore-errors (previous-line 5))))
+
+(defun indent-region-or-buffer ()
+  "Indent a region if selected, otherwise the whole buffer."
+  (interactive)
+  (save-excursion
+    (if (region-active-p)
+        (progn
+          (indent-region (region-beginning) (region-end))
+          (message "Indented selected region."))
+      (progn
+        (indent-region (point-min) (point-max))
+        (message "Indented buffer.")))))
+
+
+(use-package anzu
+  :init (progn
+          (global-anzu-mode +1)
+          (global-set-key (kbd "M-c") 'anzu-query-replace-regexp)
+          (diminish 'anzu-mode)))
+
+
+(setq echo-keystrokes 0.1)
+
+(defun join-lines (arg)
+  (interactive "p")
+  (end-of-line)
+  (delete-char 1)
+  (delete-horizontal-space)
+  (insert " "))
+
+(defun move-line-up ()
+  (interactive)
+  (let ((col (current-column)))
+    (save-excursion
+      (forward-line)
+      (transpose-lines -1))
+    (move-to-column col)))
+
+(defun post-to-s3 ()
+  (interactive)
+  (compile (concat "s3cmd -P put " (buffer-file-name) " s3://imthings")))
+
+(defun publish-file ()
+  (interactive)
+  (let ((content (replace-regexp-in-string
+                  "\n$" ""
+                  (shell-command-to-string (concat "~/bin/producer.rb " (buffer-file-name)))))
+        (output-name (concat "http://imthings.s3.amazonaws.com/"
+                             (file-name-base (buffer-file-name))
+                             ".html")))
+    (progn
+      (compile (concat "s3cmd -P put " content " s3://imthings"))
+      (kill-new output-name))))
+
+
+(defun open-previous-line (arg)
+  "Open a new line before the current one.
+     See also `newline-and-indent'."
+  (interactive "p")
+  (beginning-of-line)
+  (open-line arg)
+  (indent-according-to-mode))
+
+(global-set-key (kbd "M-j") 'join-lines)
+(global-set-key (kbd "M-g") 'goto-line)
+(global-set-key (kbd "C-S-o") 'move-line-up)
+(global-set-key (kbd "C-o") 'open-previous-line)
+
+
+(defun cleanup-buffer-safe ()
+  "Perform a bunch of safe operations on the whitespace content of a buffer.
+Does not indent buffer, because it is used for a `before-save-hook`, and that
+might be bad."
+  (interactive)
+  (untabify (point-min) (point-max))
+  (delete-trailing-whitespace)
+  (set-buffer-file-coding-system 'utf-8))
+
+(add-hook 'before-save-hook 'cleanup-buffer-safe)
+
+(use-package idomenu
+  :bind ("C-'" . idomenu))
+;;
+;; (use-package fill-column-indicator
+;;   :config (setq fci-rule-column 120)
+;;   :init (add-hook 'prog-mode-hook 'fci-mode))
+
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-use)))))))
+
+(use-package js2-mode
+  :init (progn
+          (setq-default js2-basic-offset 4)
+          (setq-default js-indent-level 4)
+          (setq-default js2-mode-indent-ignore-first-tab t)
+          (setq-default js2-show-parse-errors nil)
+          (setq-default js2-strict-inconsistent-return-warning nil)
+          (setq-default js2-strict-var-hides-function-arg-warning nil)
+          (setq-default js2-strict-missing-semi-warning nil)
+          (setq-default js2-strict-trailing-comma-warning nil)
+          (setq-default js2-strict-cond-assign-warning nil)
+          (setq-default js2-strict-var-redeclaration-warning nil)
+          (setq-default js2-global-externs
+                        '("module" "require" "__dirname" "process" "console" "define"
+                          "JSON" "$" "_" "Backbone" ))))
+
+
+(when (window-system)
+  (set-default-font "Fira Code"))
+(let ((alist '((33 . ".\\(?:\\(?:==\\)\\|[!=]\\)")
+               (35 . ".\\(?:[(?[_{]\\)")
+               (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
+               (42 . ".\\(?:\\(?:\\*\\*\\)\\|[*/]\\)")
+               (43 . ".\\(?:\\(?:\\+\\+\\)\\|\\+\\)")
+               (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
+               (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=]\\)")
+               (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
+               (58 . ".\\(?:[:=]\\)")
+               (59 . ".\\(?:;\\)")
+               (60 . ".\\(?:\\(?:!--\\)\\|\\(?:\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[/<=>|-]\\)")
+               (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
+               (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
+               (63 . ".\\(?:[:=?]\\)")
+               (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
+               (94 . ".\\(?:=\\)")
+               (123 . ".\\(?:-\\)")
+               (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
+               (126 . ".\\(?:[=@~-]\\)")
+               )
+             ))
+  (dolist (char-regexp alist)
+    (set-char-table-range composition-function-table (car char-regexp)
+                          `([,(cdr char-regexp) 0 font-shape-gstring]))))
+
+(set-frame-font "Fira Code 13")
+;;(set-frame-font "Source Code Pro 13")
+
+(use-package smartparens
+  :config (progn
+            (require 'smartparens-config)
+            (smartparens-global-mode t))
+  (defun handle-curlys (id action context)
+    (when (eq action 'insert)
+      (newline)
+      (newline)
+      (indent-according-to-mode)
+      (previous-line)
+      (indent-according-to-mode)))
+
+  (sp-local-pair 'go-mode "{" nil :post-handlers '(:add handle-curlys))
+  (sp-local-pair 'js2-mode "{" nil :post-handlers '(:add handle-curlys))
+  (defun my-elixir-do-end-close-action (id action context)
+    (when (eq action 'insert)
+      (newline-and-indent)
+      (previous-line)
+      (indent-according-to-mode)))
+
+  (sp-with-modes '(elixir-mode)
+    (sp-local-pair "do" "end"
+                   :when '(("SPC" "RET"))
+                   :post-handlers '(:add my-elixir-do-end-close-action)
+                   :actions '(insert))))
+
+;; (use-package rainbow-identifiers
+;;   :init (add-hook 'prog-mode-hook 'rainbow-identifiers-mode))
+(require 'sane-term)
+(global-set-key (kbd "C-x t") 'sane-term)
+(global-set-key (kbd "C-x T") 'sane-term-create)
+(global-set-key (kbd "M-z") 'undo)
+
+(load-theme 'base16-harmonic16-dark t)
+
+(setq column-number-mode t)
+
+
+(use-package org
+  :init (progn
+          (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+          (global-set-key "\C-cl" 'org-store-link)
+          (global-set-key "\C-ca" 'org-agenda)))
+
+(defun add-statistics ()
+  (interactive)
+  (shell-command-to-string
+   (concat "echo \"" (format-time-string "%s") "," (buffer-file-name) "\" >> ~/Dropbox/notes/actionstats.csv")))
+
+(add-hook 'after-save-hook 'add-statistics)
